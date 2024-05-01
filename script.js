8e59f2cc0e067bf8c06e86fdd272a03107c7660d
Brandon L <bimk21@gmail.com>
2024-05-01 13:34:32 -0600

    first push for number guesser

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!';
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 19;
+
+// const guessConst = (document.querySelector('.guess').value = 17);
+// console.log(document.querySelector('.guess').value);
+// console.log(guessConst);
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
