1464dd347d88fc2a2ef946290ba1e070adabe726
Brandon L <bimk21@gmail.com>
2024-05-02 14:57:26 -0600

    third push

--- script.js
+++ script.js
@@ -13,6 +13,7 @@
 
 const randomNumber = Math.trunc(Math.random() * 20);
 document.querySelector('.number').textContent = randomNumber;
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guessOutput = Number(document.querySelector('.guess').value);
@@ -22,8 +23,22 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guessOutput === randomNumber) {
     document.querySelector('.message').textContent = 'Correct!';
   } else if (guessOutput > randomNumber) {
-    document.querySelector('.message').textContent = 'Too High';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game Over!';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guessOutput < randomNumber) {
-    document.querySelector('.message').textContent = 'Too Low';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game Over!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
