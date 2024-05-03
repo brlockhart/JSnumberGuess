7f0adf98c71a3424ed52ed9377c7cb64b717e5d8
Brandon L <bimk21@gmail.com>
2024-05-03 12:15:22 -0600

    12:15 on 5-3-24

--- script.js
+++ script.js
@@ -12,16 +12,21 @@
 // console.log(guessConst);
 
 const randomNumber = Math.trunc(Math.random() * 20);
-document.querySelector('.number').textContent = randomNumber;
 let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guessOutput = Number(document.querySelector('.guess').value);
   console.log(guessOutput);
+  //when there is no input
   if (!guessOutput) {
     document.querySelector('.message').textContent = 'Please enter a number';
+    //when the correct number is guessed
   } else if (guessOutput === randomNumber) {
+    document.querySelector('.number').textContent = randomNumber;
     document.querySelector('.message').textContent = 'Correct!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    //when the guess is too high
   } else if (guessOutput > randomNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too High';
@@ -31,9 +36,10 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'Game Over!';
       document.querySelector('.score').textContent = 0;
     }
+    //when the guess is too low
   } else if (guessOutput < randomNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too High';
+      document.querySelector('.message').textContent = 'Too Low';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
@@ -42,3 +48,9 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+/*
+const pOrange = (document.querySelectorAll('p').style.color = 'orange');
+array.forEach(function (pOrange) {
+  console.log(pOrange);
+});
+*/
