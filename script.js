d537bc6e8561cd567a98a15294a251b2405c56c9
Brandon L <bimk21@gmail.com>
2024-05-11 23:24:19 -0600

    reduction in repeated code

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = 'Correct Number!';
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 19;
-
-// const guessConst = (document.querySelector('.guess').value = 17);
-// console.log(document.querySelector('.guess').value);
-// console.log(guessConst);
-
 let randomNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+}
+const displayScore = function (scoreArg) {
+    document.querySelector('.score').textContent = scoreArg;
+}
 
 document.querySelector('.check').addEventListener('click', function () {
     const guessOutput = Number(document.querySelector('.guess').value);
-    console.log(guessOutput);
+
     //when there is no input
     if (!guessOutput) {
-        document.querySelector('.message').textContent =
-            'You must enter a number...';
+        displayMessage('You must enter a number...');
         //when the correct number is guessed
     } else if (guessOutput === randomNumber) {
         document.querySelector('.number').textContent = randomNumber;
-        document.querySelector('.message').textContent = 'Correct!';
+        displayMessage('Correct!')
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
         if (highScore < score) {
@@ -36,55 +30,23 @@ document.querySelector('.check').addEventListener('click', function () {
     //reducing repeated code
     else if (guessOutput !== randomNumber) {
         if (score > 1) {
-            document.querySelector('.message').textContent =
-                guessOutput > randomNumber ? 'Too High' : 'Too Low';
+            displayMessage(guessOutput > randomNumber ? 'Too High' : 'Too Low');
             score--;
-            document.querySelector('.score').textContent = score;
+            displayScore(score);
         } else {
-            document.querySelector('.message').textContent = 'Game Over!';
-            document.querySelector('.score').textContent = 0;
+            displayMessage('Game Over!');
+            displayScore(0);
         }
     }
 });
-  //when the guess is too high
-  //   } else if (guessOutput > randomNumber) {
-  //     if (score > 1) {
-  //       document.querySelector('.message').textContent = 'Too High';
-  //       score--;
-  //       document.querySelector('.score').textContent = score;
-  //     } else {
-  //       document.querySelector('.message').textContent = 'Game Over!';
-  //       document.querySelector('.score').textContent = 0;
-  //     }
-  //     //when the guess is too low
-  //   } else if (guessOutput < randomNumber) {
-  //     if (score > 1) {
-  //       document.querySelector('.message').textContent = 'Too Low';
-  //       score--;
-  //       document.querySelector('.score').textContent = score;
-  //     } else {
-  //       document.querySelector('.message').textContent = 'Game Over!';
-  //       document.querySelector('.score').textContent = 0;
-  //     }
-  //   }
-  //});
-  /*
-    //trying to change the color of all paragraphs to orange. a loop is needed for this
-    const pOrange = (document.querySelectorAll('p').style.color = 'orange');
-    array.forEach(function (pOrange) {
-      console.log(pOrange);
-    });
-    */
   //functionality for "again" button
   document.querySelector('.again').addEventListener('click', function () {
     score = 20;
     randomNumber = Math.trunc(Math.random() * 20) + 1;
-    document.querySelector('.message').textContent = 'Start guessing...';
-    document.querySelector('.score').textContent = score;
+    displayMessage('Start guessing...');
+    displayScore(score);
     document.querySelector('.number').textContent = '?';
     document.querySelector('.guess').value = '';
     document.querySelector('body').style.backgroundColor = '#222';
     document.querySelector('.number').style.width = '15rem';
-    //location.reload();
-  })
-;
+  });
